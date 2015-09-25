cbacb7c8edfba6785a34a56c74467d4310fb0522
Nicolas Berger <nberger1999@yahoo.com>
2015-09-25 09:26:03 +0200

    Add scripts, config file update

---
+++ delete.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env ipython
+
+import sys,shelve
+from snptools import Config
+
+if sys.argv[1] == 'snp' :
+  filename = Config().snp_db_file
+elif sys.argv[1] == 'genoset' :
+  filename = Config().snp_genoset_file
+else :
+  print "Usage : delete.py [snp|genoset] name"
+  sys.exit(0)
+
+shelf = shelve.open(filename)
+if not sys.argv[2] in shelf:
+  print "No entry '%s' in database" % sys.argv[2]
+else :
+  del shelf[sys.argv[2]]
---
+++ download.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env ipython
+
+from snptools import Downloader
+Downloader().download()
+
+
